71e94a52c489b466257e425317d49107254f4e5d
JulioC354R <julyocesarplay@gmail.com>
2026-03-20 19:05:58 -0300

    take off the comments

--- api/src/database/database.types.ts
+++ api/src/database/database.types.ts
@@ -3,10 +3,10 @@ export type Database = ReturnType<typeof drizzle>;
 import type { DatabaseError } from 'pg';
 
 export type DrizzleQueryError = {
-  name: string; // geralmente "DrizzleQueryError"
-  message: string; // mensagem do erro
-  query: string; // query SQL que falhou
-  params: unknown[]; // parâmetros passados na query
-  cause?: DatabaseError; // o erro real do Postgres
+  name: string;
+  message: string;
+  query: string;
+  params: unknown[];
+  cause?: DatabaseError;
   stack?: string;
 };
