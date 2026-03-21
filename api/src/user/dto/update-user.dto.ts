22c62f93dd17c7d0425e77fb357e022c889254ae
JulioC354R <julyocesarplay@gmail.com>
2026-03-20 22:46:22 -0300

    refactor: clean up import statements in update-user.dto.ts

--- api/src/user/dto/update-user.dto.ts
+++ api/src/user/dto/update-user.dto.ts
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto'; 
+import { CreateUserDto } from './create-user.dto';
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   status: 'active' | 'inactive' | 'pending';
   created_at: string;
